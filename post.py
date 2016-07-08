b1204de6b2bec3e3202411b3d224526b9ef0457b
lorajdavis <username@users.noreply.github.com>
2016-07-08 11:42:03 -0400

    Initial commit

---
+++ post.py
@@ -0,0 +1,18 @@
+import json
+import requests
+
+baseURL = 'http://YOURBACKENDURL:8089'
+user='YOURUSERNAME'
+password='YOURPASSWORD'
+
+auth = requests.post(baseURL + '/users/'+user+'/login?password='+password).json()
+session = auth["session"]
+headers = {'X-ArchivesSpace-Session':session, 'Content_Type':'application/json'}
+
+with open('YOURJSONFILE.json') as jsonfile:
+    for line in jsonfile:
+	json_obj = json.loads(line)
+        uri_print = json_obj['uri']
+	json_string = json.dumps(json_obj)
+	resources = requests.post(baseURL + uri_print, headers=headers, data=json_string).json()
+	print digital_objects
