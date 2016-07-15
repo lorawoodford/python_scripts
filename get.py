0240a71ac3874c69874ff3d8850b5b62b35cb0ca
lorajdavis <username@users.noreply.github.com>
2016-07-15 12:15:01 -0400

    Update get.py
    Pretty print JSON.

--- get.py
+++ get.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-baseURL = 'http://BACKENDURL:8089'
+baseURL = 'http://YOURBACKEND:8089'
 user='USERNAME'
 password='PASSWORD'
 
@@ -9,8 +9,8 @@ auth = requests.post(baseURL + '/users/'+user+'/login?password='+password).json(
 session = auth["session"]
 headers = {'X-ArchivesSpace-Session':session, 'Content_Type':'application/json'}
 
-endpoint = '/repositories/3/resources'
+endpoint = '/repositories/3/accessions'
 arguments = '?page=1&page_size=3000'
 
 output = requests.get(baseURL + endpoint + arguments, headers=headers).json()
-print output
+print(json.dumps(output, indent=2))
