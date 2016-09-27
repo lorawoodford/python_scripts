cf520800daacd915564150576d46b4a483500699
Lora Davis <lorajdavis@gmail.com>
2016-09-27 13:13:27 -0400

    Abstracting secrets for all

--- get.py
+++ get.py
@@ -11,7 +11,7 @@ session = auth["session"]
 headers = {'X-ArchivesSpace-Session':session, 'Content_Type':'application/json'}
 
 endpoint = '/repositories/3/accessions'
-arguments = '?page=1&page_size=10'
+arguments = '?page=1&page_size=3000'
 
 output = requests.get(baseURL + endpoint + arguments, headers=headers).json()
 print(json.dumps(output.get('results'), indent=2))
--- post.py
+++ post.py
@@ -1,9 +1,10 @@
+import secrets
 import json
 import requests
 
-baseURL = 'http://YOURBACKENDURL:8089'
-user='YOURUSERNAME'
-password='YOURPASSWORD'
+baseURL = 'http://'+secrets.backendurl+':8089'
+user=secrets.user
+password=secrets.password
 
 auth = requests.post(baseURL + '/users/'+user+'/login?password='+password).json()
 session = auth["session"]
