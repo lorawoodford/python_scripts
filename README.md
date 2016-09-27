ca5dbd756da3827667978b9a5503cae598bbf883
Lora Davis <lorajdavis@gmail.com>
2016-09-27 13:11:07 -0400

    Abstracting secrets
    Moved backendurl, user, and passwords to separate secrets.py

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # python_scripts
 For interacting with the ArchivesSpace API.
+
+# Authenticating with secrets.py
+These scripts call a separate secrets.py that should be in the following format:
+
+backendurl='YOURBACKENDURL'
+user='YOURUSER'
+password='YOURPASSWORD'
--- get.py
+++ get.py
@@ -1,16 +1,17 @@
+import secrets
 import json
 import requests
 
-baseURL = 'http://YOURBACKEND:8089'
-user='USERNAME'
-password='PASSWORD'
+baseURL = 'http://'+secrets.backendurl+':8089'
+user=secrets.user
+password=secrets.password
 
 auth = requests.post(baseURL + '/users/'+user+'/login?password='+password).json()
 session = auth["session"]
 headers = {'X-ArchivesSpace-Session':session, 'Content_Type':'application/json'}
 
 endpoint = '/repositories/3/accessions'
-arguments = '?page=1&page_size=3000'
+arguments = '?page=1&page_size=10'
 
 output = requests.get(baseURL + endpoint + arguments, headers=headers).json()
 print(json.dumps(output.get('results'), indent=2))
